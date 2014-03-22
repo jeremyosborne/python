49d89c2dfc49200a6da5eead3013380c19af876c
Jeremy Osborne <jeremywosborne@gmail.com>
2014-03-22 10:54:38 -0700

    More schtuff.

---
+++ examples/datatypes_lab/sequences.py
@@ -0,0 +1,28 @@
+# Samples are small. Prove your answers by printing.
+
+
+
+# Call list() on a string.
+# How does the list constructor work?
+
+
+
+# Call list() on a dict. 
+# What is the output and why?
+
+
+
+# Convert this list to a tuple.
+l3 = [1, 2, 3]
+
+# Compare the list [1, 2, 3] with the tuple (1, 2, 3).
+# Why are they not equal?
+
+
+
+# Compare the following two lists (different addresses in memory).
+# Why are they equal?
+l4 = [1, 2, 3]
+l5 = [1, 2, 3]
+print l4 == l5
+
---
+++ examples/datatypes_lab/solution/sequences.py
@@ -0,0 +1,35 @@
+# Samples are small. Prove your answers by printing.
+
+
+
+# Call list() on a string.
+# How does the list constructor work?
+l = list("hello world")
+print l
+
+
+
+# Call list() on a dict. 
+# What is the output and why?
+l2 = list({"hello": "world", "and": "universe"})
+print l2
+
+
+
+# Convert this list to a tuple.
+l3 = [1, 2, 3]
+t = tuple(l3)
+print t
+
+# Compare the list [1, 2, 3] with the tuple (1, 2, 3).
+# Why are they not equal?
+print t == l3
+
+
+
+# Compare the following two lists (different addresses in memory).
+# Why are they equal?
+l4 = [1, 2, 3]
+l5 = [1, 2, 3]
+print l4 == l5
+
